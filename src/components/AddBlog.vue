7853187aec799e08a2f1f45d6932b1a2fea6571f
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-09-04 17:39:10 +0200

    select commit

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -4,10 +4,31 @@
       <form>
         <label class="label">Blog Title:</label>
 
-        <input type="text" v-model="blog.title" required>
+        <input type="text" v-model.lazy="blog.title" required>
         <label for="">Blog Content:</label>
-        <textarea name="name" rows="8" cols="80" v-model="blog.content"></textarea>
+        <textarea name="name" rows="8" cols="80" v-model.lazy="blog.content"></textarea>
+        <div id="checkboxs">
+          <label class="label">check 1 :</label>
+          <input type="checkbox" name="" value="check 1"
+            v-model="blog.categories">
+          <label class="label">check 2:</label>
+          <input type="checkbox" name="" value="check 2"
+            v-model="blog.categories">
+          <label class="label">check 3:</label>
+          <input type="checkbox" name="" value="check 3"
+            v-model="blog.categories">
+          <label class="label">check 4:</label>
+          <input type="checkbox" name="" value="check 4"
+            v-model="blog.categories">
+          <label class="label">check 5:</label>
+          <input type="checkbox" name="" value="check 5"
+            v-model="blog.categories">
+        </div>
+        <label>Slect One</label>
+        <select v-model="blog.author" name="">
+            <option v-for="author in blog.authors">{{author}}</option>
 
+        </select>
       </form>
 
 
@@ -16,6 +37,10 @@
         <h3>Preview Blog</h3>
         <p>Blog Title: {{blog.title}}</p>
         <p>Blog Content: {{blog.content}}</p>
+
+        <ul>
+          <li v-for="category in blog.categories">{{category}}</li>
+        </ul>
       </div>
   </div>
 </template>
@@ -26,7 +51,10 @@
       return{
         blog:{
           title:"",
-          content:""
+          content:"",
+          categories:[],
+          author:"",
+          authors:['ahmed','sayed','hahah','dhdhgdg']
         }
 
       }
@@ -57,6 +85,13 @@
   }
   h3{
     margin: 10px;
+  }
+  #checkboxs input{
+    display:inline-block;
+    margin-right: 10px;
+  }
+  #checkboxs label{
+    display: inline-block;
   }
 
 </style>
